b0be9f683e389e732a60e0d9d7240886f5bce1af
Chinonyerem <kaewiz166@gmail.com>
2026-02-26 13:29:17 +0100

    updates

--- src/app/services/web-mobile-vas/page.tsx
+++ src/app/services/web-mobile-vas/page.tsx
@@ -1,13 +1,13 @@
 import HeroBanner from "@/components/Herobanner";
 import HowWeWorkSection from "@/components/Howweworksection";
 import WebMobileHero from "@/pages/web-and-mobile-vas/web&mobilehero";
-// import ServiceDetailSection from "@/pages/web-and-mobile-vas/servicedetails";
+import ServiceDetailSection from "@/pages/web-and-mobile-vas/servicedetails";
 
 export default function WebAndMobileVAS() {
     return (
        <main className="bg-white">
         <WebMobileHero categoryTitle="Web & Mobile VAS" />
-        {/* <ServiceDetailSection /> */}
+        <ServiceDetailSection />
         <HowWeWorkSection />
         <HeroBanner
         title=" Set Up Mobile Billing"
